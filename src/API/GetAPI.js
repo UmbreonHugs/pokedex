90efe3cd186992c895dbadcd8fe5bb4672313738
Luigi <thegaminggodlou@gmail.com>
2019-03-07 01:15:19 -0800

    Created React App, installed the PokeAPI wrapper to cache all requests

---
+++ src/API/GetAPI.js
@@ -0,0 +1,6 @@
+const Pokedex = require('pokeapi-js-wrapper');
+const P = new Pokedex.Pokedex();
+P.getPokemonByName('eevee') // with Promise
+  .then(function(response) {
+    console.log(response);
+  });
